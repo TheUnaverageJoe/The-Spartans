a76278d0f9a16473cfad9dd5c86373dfdc87129a
TheUnaverageJoe <jobrcart@ucsc.edu>
2022-01-11 00:10:37 -0800

    1/11 changes

--- Spartans-Unity-Project/Assets/Scripts/Player.cs
+++ Spartans-Unity-Project/Assets/Scripts/Player.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
     private Animator anim;
     private Rigidbody rb;
