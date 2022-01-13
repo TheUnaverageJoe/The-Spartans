164ae9af066b02f585991543c9fb4175df215af5
TheUnaverageJoe <jobrcart@ucsc.edu>
2022-01-12 17:03:13 -0800

    Changes

--- Spartans-Unity-Project/Assets/Scripts/NetworkManagerScript.cs
+++ Spartans-Unity-Project/Assets/Scripts/NetworkManagerScript.cs
@@ -5,35 +5,43 @@ using Unity.Netcode;
 
 public class NetworkManagerScript : MonoBehaviour
 {
+    private static int GUI_BUTTON_WIDTH = 465;
+    private static int GUI_BUTTON_HEIGHT = 400;
+    // private Rect GUI_AREA = new Rect(Screen.width/2 - GUI_BUTTON_WIDTH/2,
+    //                                  Screen.height/2-50,
+    //                                  GUI_BUTTON_WIDTH,
+    //                                  GUI_BUTTON_HEIGHT);
+    private Rect GUI_AREA = new Rect(0,0, GUI_BUTTON_WIDTH, GUI_BUTTON_HEIGHT);
     void OnGUI()
         {
-            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
-            {
-                StartButtons();
+            GUIStyle customButton = new GUIStyle("button");
+            customButton.fontSize = 40;
+            GUILayout.BeginArea(GUI_AREA);
+            GUILayout.BeginHorizontal("box");
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer){
+                StartButtons(customButton);
+            }else{
+                StatusLabels(customButton);
             }
-            else
-            {
-                StatusLabels();
-                //SubmitNewPosition();
-            }
-
+            GUILayout.EndHorizontal();
             GUILayout.EndArea();
         }
 
-        static void StartButtons()
+        static void StartButtons(GUIStyle customButton)
         {
-            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
-            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+
+            if (GUILayout.Button("Host", customButton, GUILayout.Width(150), GUILayout.Height(100))) NetworkManager.Singleton.StartHost();
+            if (GUILayout.Button("Client", customButton, GUILayout.Width(150), GUILayout.Height(100))) NetworkManager.Singleton.StartClient();
+            if (GUILayout.Button("Server", customButton, GUILayout.Width(150), GUILayout.Height(100))) NetworkManager.Singleton.StartServer();
         }
 
-        static void StatusLabels(){
+        static void StatusLabels(GUIStyle customButton){
+            customButton.fontSize = 24;
             var mode = NetworkManager.Singleton.IsHost ?
                 "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
 
             GUILayout.Label("Transport: " +
-                NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
-            GUILayout.Label("Mode: " + mode);
+                NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name, customButton);
+            GUILayout.Label("Mode: " + mode, customButton);
         }
 }
--- Spartans-Unity-Project/Assets/Scripts/Player.cs
+++ Spartans-Unity-Project/Assets/Scripts/Player.cs
@@ -12,6 +12,7 @@ public class Player : NetworkBehaviour
     [SerializeField] private bool isGrounded;
     [SerializeField] private float MAX_SPEED = 10.0f;
     [SerializeField] private float MOVE_SPEED = 0.1f;
+    [SerializeField] private float X_SENS = 1.0f;
 
     //------------------------------
     // Start is called before the first frame update
@@ -44,11 +45,14 @@ public class Player : NetworkBehaviour
             anim.SetBool("isWalking", false);//change animator variable for walking and Idle
         }
 
-        
         if(Input.GetKeyDown("f")){
             anim.SetTrigger("throw");
         }
 
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(Cursor.visible) MouseLock(true);
+            else MouseLock(false);
+        }
 
     }
 
@@ -56,23 +60,33 @@ public class Player : NetworkBehaviour
         
         if(!IsLocalPlayer) return;//Ensure Each client only moves there own player,
         //I dont understand why this is neccessary given each player should have their own script instance for the player character and own game window in focus
-        Debug.Log("In Fixed Update");
+        //Debug.Log("In Fixed Update");
 
-        //binaray conversion, layer mask should be an int, convert it to binary to see which layers are true(1) or false(0)
+        //binary conversion, layer mask should be an int(Layermask is parameter of OverlapBox), convert it to binary to see which layers are true(1) or false(0)
         //00000000000000000000000000000001(Binary) = 1(Integer)
         // summation of 2^(Layer Number) for all layers that should be used for overlap
         //aka layer 0 will trigger OverlapBox because 2^0 = 1
 
         //only detect objects on layer 0, "default" layer
-        Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, new Vector3(1, 0.5f, 0.5f), Quaternion.identity, 1, QueryTriggerInteraction.Ignore);
+        Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, new Vector3(1, 0.5f, 0.5f),
+                                                     Quaternion.identity, 1, QueryTriggerInteraction.Ignore);
         if(hitColliders.Length == 0) isGrounded = false;
         if(hitColliders.Length > 0) isGrounded = true;
 
         Move();
-
-        
+        Look();
     }
 
+    void OnApplicationFocus(bool hasFocus){
+        if(hasFocus){
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }else{
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+    //OnDrawGizmos is being usec purly for debugging  purposes to see where the hitbox is in world space
     private void OnDrawGizmos() {
      Gizmos.color = Color.red;
      //Use the same vars you use to draw your Overlap SPhere to draw your Wire Sphere.
@@ -80,10 +94,9 @@ public class Player : NetworkBehaviour
  }
 
     void Move(){
-
         if(!isGrounded) return; //we arent touching the floor, we can't move in the air
         //if(rb.velocity.magnitude >= MAX_SPEED) return; //Dont allow character to be too fast
-        Debug.Log("Velocity: " + rb.velocity);
+        //Debug.Log("Velocity: " + rb.velocity);
 
         //store Y axis component of velocity because it should remain unchanged by player input aside from jumping
         float YAXIS = rb.velocity.y;
@@ -108,13 +121,25 @@ public class Player : NetworkBehaviour
             rb.velocity = yAxis;
         }
 
-        //
+        //Left and right strafe movement
         if(Input.GetAxis("Horizontal")>0){
 
         }
     }
 
     void Look(){
+        transform.Rotate(0,Input.GetAxis("Mouse X")*X_SENS,0);
+        //transform.localEulerAngles.y += Input.GetAxis("Mouse X") * X_SENS;
+    }
 
+    void MouseLock(bool Lock){
+        if(Lock){
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            return;
+        }else if(!Lock){
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 }
