a34bb906f6db76475fdb304efdb9cf1d602cd40b
TheUnaverageJoe <jobrcart@ucsc.edu>
2022-06-16 23:30:09 -0700

    Started adding connection screen

--- Spartans-Unity-Project/Assets/Scripts/GameManager.cs
+++ Spartans-Unity-Project/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.UI;
+using TMPro;
 
 using Spartans.Players;
 
@@ -9,43 +11,51 @@ namespace Spartans{
     public class GameManager : MonoBehaviour
     {
         private List<Player> _players = new List<Player>();
+        private bool _pregame = true;
+        private bool _inGame = false;
+        [SerializeField] private GameObject connectionUI;
+        private TMP_InputField _input;
 
         void Start(){
-            
-            NetworkManager.Singleton.OnClientConnectedCallback += (id) => {
-                if(NetworkManager.Singleton.IsServer){
-                    Player newPlayer;
-                    NetworkManager.Singleton.ConnectedClients[id].PlayerObject.TryGetComponent<Player>(out newPlayer);
-                    //if(NetworkManager.Singleton.IsServer){
-                    //    newPlayer.Start();
-                    //}
-                    _players.Add(newPlayer);
-                    print("----Players----- ");
-                    foreach(Player client in _players){
-                        print("Player: " + client.playerName);
-                    }
-                    print("-----------------");
 
-                }
-            };
-            
+            if(NetworkManager.Singleton.IsServer) NetworkManager.Singleton.OnClientConnectedCallback += AddPlayer;
+
+            _input = connectionUI.GetComponentInChildren<TMP_InputField>();
         }
 
         void FixedUpdate(){
-            if(!NetworkManager.Singleton.IsServer) return;
+            if(_pregame){
+                connectionUI.SetActive(true);
+            }else{
+                connectionUI.SetActive(false);
+            }
+        }
+
+        private void AddPlayer(ulong id){
+            {
+                Player newPlayer;
+                NetworkManager.Singleton.ConnectedClients[id].PlayerObject.TryGetComponent<Player>(out newPlayer);
+                //if(NetworkManager.Singleton.IsServer){
+                //    newPlayer.Start();
+                //}
+                _players.Add(newPlayer);
+                print("----Players----- ");
+                foreach(Player client in _players){
+                    print("Player: " + client.playerName);
+                }
+                print("-----------------");
+            }
+        }
+
+        public void StartServer(){
+            NetworkManager.Singleton.StartServer();
+        }
+        public void StartHost(){
+            NetworkManager.Singleton.StartHost();
+        }
+        public void JoinGame(){
             
-            if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient )
-            {
-                
-            }
-            else
-            {
-                //Debug.Log("Hey look I made it");
-                
-                //NetworkObject playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
-                //PlayerMovement player = playerObject.GetComponent<PlayerMovement>();
-                //player.Move();
-            }
+            NetworkManager.Singleton.StartClient();
         }
     }
 }
