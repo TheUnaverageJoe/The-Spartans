f0777b49cd2f7ae61ccbbefa9af01b46c776bade
TheUnaverageJoe <jcarter94043@gmail.com>
2022-09-27 21:36:10 -0700

    Working to add menu scene

--- Spartans-Unity-Project/Assets/Scripts/GameManager.cs
+++ Spartans-Unity-Project/Assets/Scripts/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.Events;
 
@@ -14,27 +14,39 @@ using System;
 namespace Spartans{
     public class GameManager : MonoBehaviour
     {
+        private const string MENU_SCENE_NAME = "MainMenu";
         PlayerCanvasManager _playerCanvasManager;
         //private List<Player> _players = new List<Player>();
         [SerializeField] private GameObject connectionUI;
+        [SerializeField] private GameObject _backButton;
         private TMP_InputField _input;
         private UnityTransport connection;
         //private PanelManager.ConnectionInfo info;
-        public UnityEvent _joinedGame;
+        public UnityEvent joinedGame;
+        public System.Action onClickBack;
+        enum States{
+            Select,
+            InSession,
+            PostGame
+        }
+        bool connected = false;
+        States activeState = States.Select;
 
         void Start(){
             //if(NetworkManager.Singleton.IsServer)
                 //NetworkManager.Singleton.OnClientConnectedCallback += RandomizeSpawn;
             //NetworkManager.Singleton.OnClientConnectedCallback += RequestAddPlayerServerRPC;
 
-            _joinedGame = new UnityEvent();
-            _joinedGame.AddListener(JoinGameCallback);
+            joinedGame = new UnityEvent();
+            joinedGame.AddListener(JoinGameCallback);
+            onClickBack += onClickBackCallback;
 
             //_playerCanvasManager = FindObjectOfType<PlayerCanvasManager>();
             //_playerCanvasManager.Init();
             
             _input = connectionUI.GetComponentInChildren<TMP_InputField>();
             connection = NetworkManager.Singleton.GetComponent<UnityTransport>();
+
             
             Physics.gravity = new Vector3(0, -20f, 0);
         }
@@ -42,11 +54,11 @@ namespace Spartans{
 
         public void StartServer(){
             NetworkManager.Singleton.StartServer();
-            _joinedGame.Invoke();
+            joinedGame.Invoke();
         }
         public void StartHost(){
             NetworkManager.Singleton.StartHost();
-            _joinedGame.Invoke();
+            joinedGame.Invoke();
         }
         public void JoinGame(){
             if(_input.text.Length < 7 || _input.text == null){
@@ -55,13 +67,30 @@ namespace Spartans{
                 connection.ConnectionData.Address = _input.text;
             }
             NetworkManager.Singleton.StartClient();
-            _joinedGame.Invoke();
+            joinedGame.Invoke();
 
+        }
+        public void BackButtonPressed(){
+            onClickBack?.Invoke();
         }
 
         private void JoinGameCallback(){
             connectionUI.gameObject.SetActive(false);
+            connected = true;
+            activeState = States.InSession;
+            _backButton.SetActive(false);
             //_playerCanvasManager.OnJoinGame();
+        }
+        private void onClickBackCallback(){
+            SceneManager.LoadScene(MENU_SCENE_NAME);
+        }
+
+        private void OnDestroy(){
+            if(NetworkManager.Singleton.gameObject != null){
+                Destroy(NetworkManager.Singleton.gameObject);
+            }
+            joinedGame.RemoveListener(JoinGameCallback);
+            onClickBack -= onClickBackCallback;
         }
     }
 }
